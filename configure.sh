e372c3c69ccc966cbc8b82323f2c3ffdc73dfa95
Amr Alaa <amr.alaa.fcih@gmail.com>
2022-10-25 11:48:50 +0200

    update config to colab

--- configure.sh
+++ configure.sh
@@ -1,11 +1,7 @@
 apt install openjdk-8-jdk
 NXS_HOME=/content/drive/MyDrive/Nexus-server
-sudo adduser nexus
-sudo chown -R nexus:nexus $NXS_HOME/nexus-3.42.0-01
-sudo chown -R nexus:nexus $NXS_HOME/sonatype-work
 export PATH=$PATH:$NXS_HOME/loophole:$NXS_HOME/nexus-3.42.0-01/bin
 loophole account login
-su - nexus
 
 
 
