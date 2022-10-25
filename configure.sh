52ee3a4bb0e0f6b7ee59c80b3fe305d422e90051
Amr Alaa <amr.alaa.fcih@gmail.com>
2022-10-25 11:37:15 +0200

    Modify config

--- configure.sh
+++ configure.sh
@@ -1,9 +1,9 @@
 apt install openjdk-8-jdk
-cd /content/drive/MyDrive/Nexus-server
+NXS_HOME=/content/drive/MyDrive/Nexus-server
 sudo adduser nexus
-sudo chown -R nexus:nexus nexus-3.42.0-01
-sudo chown -R nexus:nexus sonatype-work
-export PATH=$PATH:/content/drive/MyDrive/Nexus-server/loophole:/content/drive/MyDrive/Nexus-server/nexus-3.42.0-01/bin
+sudo chown -R nexus:nexus $NXS_HOME/nexus-3.42.0-01
+sudo chown -R nexus:nexus $NXS_HOME/sonatype-work
+export PATH=$PATH:$NXS_HOME/loophole:$NXS_HOME/nexus-3.42.0-01/bin
 loophole account login
 su - nexus
 
